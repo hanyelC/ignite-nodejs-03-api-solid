2a544d79a428b6190f4f4899f46261645d9677a4
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-10 15:16:20 -0300

    refactor: add dependency inversion

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,36 +1,34 @@
 import { prisma } from '@/lib/prisma'
 import { hash } from 'bcrypt'
 
-import { PrismaUsersRepository } from '@/repositories/prisma-users.repository'
-
 interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
 }
-
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
-  }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
+export class RegisterUseCase {
+  constructor(private usersRepository: any) { }
+  async execute({
     name,
     email,
-    password_hash,
-  })
+    password,
+  }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
 }
