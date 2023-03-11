f72ffbb0401190da293be9ab6df01b695963ce83
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-11 09:13:39 -0300

    feat: create authenticate controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from 'fastify'
-import { register } from './controllers/register.controller'
+
+import { authenticate } from '@/http/controllers/authenticate.controller'
+import { register } from '@/http/controllers/register.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
