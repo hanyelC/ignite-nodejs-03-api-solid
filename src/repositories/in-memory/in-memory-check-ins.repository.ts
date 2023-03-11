36382b56eeddb3852588e74ef71fbbb44cddd271
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-11 10:36:35 -0300

    feat: create check-in use-case

---
+++ src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -0,0 +1,22 @@
+import { CheckIn, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+import { CheckInsRepository } from '@/repositories/check-ins.repository'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public items: CheckIn[] = []
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn: CheckIn = {
+      id: randomUUID(),
+      created_at: new Date(),
+      gym_id: data.gym_id,
+      user_id: data.user_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null
+    }
+
+    this.items.push(checkIn)
+
+    return checkIn
+  }
+}
