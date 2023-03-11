086c96c03c6f02258302dc83ffb6b8e60040e755
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-11 08:42:22 -0300

    feat: create authenticate use-case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,33 @@
+import { UsersRepository } from '@/repositories/users.repository'
+import { User } from '@prisma/client'
+import { compare } from 'bcrypt'
+import { InvalidCredentialsError } from './errors/InvalidCredentials.error'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) { }
+
+  async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
