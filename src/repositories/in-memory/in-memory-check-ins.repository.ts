f54a7d25cce8d69ebc9d1375928e95c216c0d74f
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-13 18:26:37 -0300

    feat: fetch check-ins use-case

--- src/repositories/in-memory/in-memory-check-ins.repository.ts
+++ src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -6,6 +6,11 @@ import { CheckInsRepository } from '@/repositories/check-ins.repository'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
 
   async findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null> {
     const startOfTheDay = dayjs(date).startOf('date')
