2b618a65a8ca5924e62b3e3a527f6f003c08944e
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-14 19:23:19 -0300

    feat: create prisma check-in repository and finish prisma user repository

--- src/repositories/prisma/prisma-users.repository.ts
+++ src/repositories/prisma/prisma-users.repository.ts
@@ -1,11 +1,17 @@
-import { prisma }       from '@/lib/prisma'
-import { Prisma, User } from '@prisma/client'
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
 
 import { UsersRepository } from '@/repositories/users.repository'
 
 export class PrismaUsersRepository implements UsersRepository {
-  async findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id
+      }
+    })
+
+    return user
   }
 
   async findByEmail(email: string) {
