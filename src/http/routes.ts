152910434ea5502212b092a515710fb0aec4760b
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-15 09:56:31 -0300

    feat: create verifyJwt middleware

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,11 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from '@/http/controllers/authenticate.controller'
 import { profile }      from '@/http/controllers/profile.controller'
 import { register }     from '@/http/controllers/register.controller'
+import { verifyJwt }    from '@/http/middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
