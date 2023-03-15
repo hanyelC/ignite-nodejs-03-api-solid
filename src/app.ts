a3ab897b0e65b735ef5f8f4cae2229904144d992
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-15 14:58:35 -0300

    chore: restructure controllers folder

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import fastifyJwt   from '@fastify/jwt'
 import fastify      from 'fastify'
 import { ZodError } from 'zod'
 
-import { env }       from '@/env'
-import { appRoutes } from '@/http/routes'
+import { env }         from '@/env'
+import { usersRoutes } from '@/http/controllers/users/routes'
 
 export const app = fastify()
 
@@ -11,7 +11,7 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
