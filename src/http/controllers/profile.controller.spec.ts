ae19b50c6c655a0a4903c62e98899df6c3c3c485
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-15 14:47:12 -0300

    test: create profile e2e test

--- src/http/controllers/profile.controller.spec.ts
+++ src/http/controllers/profile.controller.spec.ts
@@ -1,3 +1,41 @@
-import { test } from 'vitest'
+import supertest                                       from 'supertest'
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
 
-test('ok', () => {})
+import { app } from '@/app'
+
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('should be able to get user profile', async () => {
+    await supertest(app.server).post('/users').send({
+      name:     'John Doe',
+      email:    'johndoe@example.com',
+      password: '123456'
+    })
+
+    const authResponse = await supertest(app.server).post('/sessions').send({
+      email:    'johndoe@example.com',
+      password: '123456'
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await supertest(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.status).toEqual(200)
+    expect(profileResponse.body).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@example.com'
+      })
+    )
+  })
+})
