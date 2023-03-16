45f0015eda474dcb4564032045a6df8299df6908
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-16 17:26:42 -0300

    feat: create refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import fastifyJwt   from '@fastify/jwt'
-import fastify      from 'fastify'
-import { ZodError } from 'zod'
+import fastifyCookie from '@fastify/cookie'
+import fastifyJwt    from '@fastify/jwt'
+import fastify       from 'fastify'
+import { ZodError }  from 'zod'
 
 import { env }            from '@/env'
 import { checkInsRoutes } from '@/http/controllers/check-ins/routes'
@@ -10,8 +11,17 @@ import { usersRoutes }    from '@/http/controllers/users/routes'
 export const app = fastify()
 
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed:     false,
+  },
+  sign: {
+    expiresIn: '10m'
+  }
 })
+
+app.register(fastifyCookie)
 
 app.register(checkInsRoutes)
 app.register(usersRoutes)
